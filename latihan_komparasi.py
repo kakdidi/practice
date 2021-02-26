53274aa210f37010dd6e1f98c1f461b11c8e14d5
kakdidi <kakdidi@gmail.com>
2021-02-26 13:25:07 +0000

    latihan komparasi

---
+++ latihan_komparasi.py
@@ -0,0 +1,15 @@
+#membuat gabungan dari rentan angka
+#+++3------10+++
+
+inputUser = float(input("masukkan angka yg bernilai kurang dari 3 : "))
+
+#memriksa nilai kurang dari 3
+isKurangDari = (inputUser < 3)
+print("Kurang dari 3 = ", isKurangDari)
+
+#memeriksa nilai lebih dari 3
+isLebihDari = (inputUser > 3)
+print("Lebih dari 3 = ", isLebihDari)
+
+isCorrect = isKurangDari or isLebihDari
+print("Angka yang anda masukkan ", isCorrect)
